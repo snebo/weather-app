87111c7a6cd0772e175094596df6df688ac97f16
Stefano <30480337+snebo@users.noreply.github.com>
2025-01-01 01:53:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# Project Name
+
+Brief project description (1-2 sentences)
+
+## Features
+
+- Key feature 1
+- Key feature 2
+- Key feature 3
+
+## Installation
+
+```bash
+# Installation steps
+npm install project-name
+```
+
+## Usage
+
+```javascript
+// Basic usage example
+import { Component } from "project-name";
+
+const instance = new Component();
+instance.init();
+```
+
+## API
+
+### Constructor
+
+```javascript
+new Component(param1, param2);
+```
+
+**Parameters:**
+
+- `param1`: Type - Description
+- `param2`: Type - Description
+
+### Methods
+
+- `methodName()`: Description
+- `methodName2()`: Description
+
+### Output
+
+```javascript
+// Example output format
+{
+    "key": "value"
+}
+```
+
+## Configuration
+
+```javascript
+// Configuration options
+{
+    "option1": "value",
+    "option2": "value"
+}
+```
+
+## CSS Classes
+
+- `.class-name`: Description
+- `.class-name2`: Description
+
+## Requirements
+
+- Requirement 1
+- Requirement 2
+
+## Contributing
+
+Instructions for contributors
+
+## License
+
+Project license information
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const DotenvWebpackPlugin = require("dotenv-webpack");
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["src/**/*"], // this updates when anythin gchanges
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // link the file here
+            // template: "./src/templates/index.html",
+        }),
+        new DotenvWebpackPlugin({
+            systemvars: true,
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ]
+    }
+}
