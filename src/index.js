2025174d9647c042d6c15c5dc88e37884dc92663
Stefano <30480337+snebo@users.noreply.github.com>
2025-01-05 23:12:42 +0100

    fixed images paths

--- src/index.js
+++ src/index.js
@@ -1,63 +1,95 @@
-import "./assets/styles.css"
-import { WEATHER_API_KEY, WEATHER_URL } from "./scripts/enviroments"
+import cloud_pic from './assets/images/cloudy.png';
+import rain_pic from './assets/images/rain.png';
+import snow_pic from './assets/images/snow.png';
+import sun_pic from './assets/images/sun.png';
+import very_cloudy from './assets/images/very_cloudy.png';
+import './assets/styles.css';
+import { WEATHER_API_KEY, WEATHER_URL } from './scripts/enviroments';
 
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("form")
-    form.addEventListener("submit", async (event) => {
-        event.preventDefault()
-        const place = document.querySelector("#place").value
-        const query_data = await fetchWeatherInfor(place)
-        console.log(processWeatherInfo(query_data))
-        changeUi(query_data)
-    })
-})
+document.addEventListener('DOMContentLoaded', () => {
+	const form = document.querySelector('form');
+	form.addEventListener('submit', async (event) => {
+		event.preventDefault();
+		const place = document.querySelector('#place').value;
+		const query_data = await fetchWeatherInfor(place);
+		const data = processWeatherInfo(query_data);
+		changeUi(data);
+	});
+});
 async function fetchWeatherInfor(address) {
-    try {
-        const response = await fetch(`${WEATHER_URL}/${address}?unitGroup=metric&key=${WEATHER_API_KEY}&contentType=json`,
-            { mode: "cors" })
-        const data = await response.json()
-        return data
-    } catch (error) {
-        console.log(error)
-    }
-
+	try {
+		const response = await fetch(
+			`${WEATHER_URL}/${address}?unitGroup=metric&key=${WEATHER_API_KEY}&contentType=json`,
+			{ mode: 'cors' }
+		);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
 }
 function processWeatherInfo(data) {
-    const location = data.resolvedAddress
-    const alearts = data.alearts
-    const current_data = {}
-    current_data.cloudcover = data.currentConditions.cloudcover
-    current_data.humidity = data.currentConditions.humidity
-    current_data.precipitation = data.currentConditions.percip
-    current_data.humidity = data.currentConditions.humidity
-    current_data.precipitation = data.currentConditions.precip
-    current_data.pressure = data.currentConditions.pressure
-    current_data.sunshine = data.currentConditions.sunshine
-    current_data.temperature = data.currentConditions.temp
-    current_data.windDirection = data.currentConditions.winddir
-    current_data.windSpeed = data.currentConditions.windspeed
-    current_data.icon = data.currentConditions.icon
-    const days = []
-    for (let i = 0; i < 3; i++) {
-        const day = {}
-        day.date = data.days[i].datetime
-        day.icon = data.days[i].icon
-        day.precipitation = data.days[i].precip
-        day.temperature = data.days[i].temp
-        day.maxTemp = data.days[i].tempmax
-        day.windDirection = data.days[i].winddir
-        day.windSpeed = data.days[i].windspeed
-        day.solarRadiation = data.days[i].solarradiation
-        data.perciptpe = data.days[i].perciptype
-        days.push(day)
-    }
-    return { location, alearts, current_data, days }
+	const location = data.resolvedAddress;
+	const alearts = data.alearts;
+	const current_data = {};
+	current_data.cloudcover = data.currentConditions.cloudcover;
+	current_data.humidity = data.currentConditions.humidity;
+	current_data.precipitation = data.currentConditions.percip;
+	current_data.humidity = data.currentConditions.humidity;
+	current_data.precipitation = data.currentConditions.precip;
+	current_data.pressure = data.currentConditions.pressure;
+	current_data.sunshine = data.currentConditions.sunshine;
+	current_data.temperature = data.currentConditions.temp;
+	current_data.windDirection = data.currentConditions.winddir;
+	current_data.windSpeed = data.currentConditions.windspeed;
+	current_data.icon = data.currentConditions.icon;
+	const days = [];
+	for (let i = 0; i < 3; i++) {
+		const day = {};
+		day.date = data.days[i].datetime;
+		day.icon = data.days[i].icon;
+		day.precipitation = data.days[i].precip;
+		day.temperature = data.days[i].temp;
+		day.maxTemp = data.days[i].tempmax;
+		day.windDirection = data.days[i].winddir;
+		day.windSpeed = data.days[i].windspeed;
+		day.solarRadiation = data.days[i].solarradiation;
+		data.perciptpe = data.days[i].perciptype;
+		days.push(day);
+	}
+	return { location, alearts, current_data, days };
 }
 
 function changeUi(data) {
-    if (!data) return // if there is no data
-    // update text ui
-    // use switch to select which imgage to load
+	console.log(data);
+	if (!data) return; // if there is no data
+	document.querySelector('.city_name').textContent = data.location;
+	document.querySelector('.temp').textContent = data.current_data.temperature;
+	document.querySelector(
+		'.humidity'
+	).textContent = `${data.current_data.humidity}%`;
+	document.querySelector(
+		'.wind'
+	).textContent = `${data.current_data.windSpeed}mph`;
+
+	// use switch to select which imgage to load
+	switch (data.current_data.icon) {
+		case 'partly-cloudy-day':
+			document.querySelector('.weather_icon').src = cloud_pic;
+			break;
+		case 'cloudy':
+			document.querySelector('.weather_icon').src = very_cloudy;
+			break;
+		case 'rain':
+			document.querySelector('.weather_icon').src = rain_pic;
+			break;
+		case 'snow':
+			document.querySelector('.weather_icon').src = snow_pic;
+			break;
+		default:
+			document.querySelector('.weather_icon').src = sun_pic;
+			break;
+	}
 }
 
 // example data
